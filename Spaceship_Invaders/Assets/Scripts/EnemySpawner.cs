47af59e042d28abd38ca8d8c3630780025604483
Thien260102 <nguyenvanthien18209@gmail.com>
2023-03-16 17:15:16 +0700

    Create method to handle Enemy Wave

--- Spaceship_Invaders/Assets/Scripts/EnemySpawner.cs
+++ Spaceship_Invaders/Assets/Scripts/EnemySpawner.cs
@@ -13,18 +13,38 @@ namespace Assets.Scripts
         [SerializeField]
         public GameObject[] enemyReference;
 
+        [SerializeField]
+        public GameObject[] ListWave;
+
+        EnemyWave enemyWave;
+
+        int currentWave = 0;
+
         private void Start()
         {
+            if(ListWave.Length > currentWave)
+            {
+                enemyWave = ListWave[currentWave++].GetComponent<EnemyWave>();
+            }
             StartCoroutine(SpawnEnemies(Variables.Enemy1, 1));
         }
 
         private IEnumerator SpawnEnemies(int Type = 0, int second = 2)
         {
-            yield return new WaitForSeconds(second);
 
-            Vector2 position = new Vector2(0, 0);
-            GameObject Instantiate_Bullet = Instantiate(enemyReference[Type], position, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)) as GameObject;
-            Instantiate_Bullet.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
+            for(int i = 0; i < enemyWave.CorrespondingEnemyQuantity.Length; i++)
+            {
+                yield return new WaitForSeconds(second);
+
+                for(int j = 0; j < enemyWave.CorrespondingEnemyQuantity[i]; j++)
+                {
+                    Vector2 position = new Vector2(-j, i);
+                    GameObject Instantiate_Enemy = Instantiate(enemyReference[i], position, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)) as GameObject;
+                    Instantiate_Enemy.transform.Rotate(0.0f, 0.0f, -90.0f, Space.Self);
+                }
+                
+            }
+            
             Debug.Log("Spawned enemy");
         }
     }
